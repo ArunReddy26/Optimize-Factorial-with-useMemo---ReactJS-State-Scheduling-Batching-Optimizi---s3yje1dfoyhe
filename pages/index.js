9447e11d7e289a2caa355fff7165f4bca4d6626c
ArunReddy26 <132929105+ArunReddy26@users.noreply.github.com>
2024-01-16 13:34:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,46 @@
 // "use client"
-import { useState } from 'react';
+import React, { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
-const Home = () => {
-  const [inputNumber, setInputNumber] = useState(5); // Set an initial number
-  const handleInputChange = (e) => setInputNumber(Number(e.target.value));
+// const Home = () => {
+//   const [inputNumber, setInputNumber] = useState(5); // Set an initial number
+//   const handleInputChange = (e) => setInputNumber(Number(e.target.value));
+
+//   return (
+//     <div>
+//       <label htmlFor="numberInput">Enter a number:</label>
+//       <input
+//         type="number"
+//         id="numberInput"
+//         value={inputNumber}
+//         onChange={handleInputChange}
+//       />
+
+//       <FactorialComponent number={inputNumber} />
+//     </div>
+//   );
+// };
+
+// export default Home;
+export default function Home() {
+   const [number, setNumber] = useState(0);
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setNumber(isNaN(value) ? 0 : value);
+  };
 
   return (
-    <div>
-      <label htmlFor="numberInput">Enter a number:</label>
-      <input
-        type="number"
-        id="numberInput"
-        value={inputNumber}
-        onChange={handleInputChange}
-      />
-
-      <FactorialComponent number={inputNumber} />
+        <div>
+        <h1>Welcome to Next.js!</h1>
+        <section>
+                <h2>Factorial Calculation</h2>
+      <input type="number" value={number} onChange={handleChange} />
+      <FactorialComponent number={number} />
+             
+            </section>
     </div>
+   
   );
-};
-
-export default Home;
-// export default function Home() {
   
      
 //   return (
@@ -39,4 +58,4 @@ export default Home;
 //             </section>
 //     </div>
 // );
-// }
+}
